27c2f20a575a7d0355a3c5745523210310a9c8a2
TracyMcGrady <245715460@qq.com>
2018-11-23 16:47:37 +0800

    为主程序添加作者和创建时间

--- src/main/java/com/itmyhome/es/TestLog4j2Kafka.java
+++ src/main/java/com/itmyhome/es/TestLog4j2Kafka.java
@@ -4,6 +4,8 @@ package com.itmyhome.es;
 import org.apache.log4j.Logger;
 
 /**
+ * @author liuchao
+ * @since 11/23/2018
  * 先启动kafka的消费服务，再启动程序，kafka使用0.8.2版本,log4j使用1.2.17版本
  * 该实例的功能是使用Log4j将程序日志实时写入Kafka
  */
